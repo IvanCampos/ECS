82f4cb1fbcd0a299665d379af13a67ae97b52fcd
Ivan Campos <sopmac21379@gmail.com>
2025-07-26 20:54:54 -0400

    initial commit

--- ECS/ImmersiveView.swift
+++ ECS/ImmersiveView.swift
@@ -15,14 +15,14 @@ struct ImmersiveView: View {
     var body: some View {
         RealityView { content in
             // Add the initial RealityKit content
-            if let immersiveContentEntity = try? await Entity(named: "Immersive", in: realityKitContentBundle) {
+            if let immersiveContentEntity = try? await Entity(named: "SkyDome", in: realityKitContentBundle) {
                 content.add(immersiveContentEntity)
             }
         }
     }
 }
 
-#Preview(immersionStyle: .full) {
-    ImmersiveView()
-        .environment(AppModel())
-}
+//#Preview(immersionStyle: .full) {
+//    ImmersiveView()
+//        .environment(AppModel())
+//}
